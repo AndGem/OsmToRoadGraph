a49862a302b4840a51a9b09cb4d4534996c0fe19
Andreas <andreasgemsa@gmx.de>
2017-02-15 21:49:01 +0100

    11-modified test

--- tests/integration_test.py
+++ tests/integration_test.py
@@ -7,15 +7,18 @@ import hashlib
 
 class IntegrationTest(unittest.TestCase):
 
-    def run_program(self):
-        print os.getcwd()
+    @classmethod
+    def setUpClass(self):
         call(["python", "run.py", "-f", "data/karlsruhe_small.osm", "-n", "p", "-c"])
 
-    def compare_hash_test(self):
-        self.run_program()
+    def pedestrian_graph_ok_test(self):
         self.assertEquals(hashlib.md5(open("data/karlsruhe_small.pypgr", 'rb').read()).hexdigest(), "d363bc83c99222ff9ddc9094e3f06664")
+
+    def pedestrian_graph_names_ok_test(self):
         self.assertEquals(hashlib.md5(open("data/karlsruhe_small.pypgr_names", 'rb').read()).hexdigest(), "2837fe0e24267d24a7b2e2335bcad687")
-        self.assertEquals(hashlib.md5(open("data/karlsruhe_small.pypgrc", 'rb').read()).hexdigest(), "bea4d974990b5e9f022e57903ae0baa5")
+
+    def pedestrian_graph_c_names_ok_test(self):
         self.assertEquals(hashlib.md5(open("data/karlsruhe_small.pypgrc_names", 'rb').read()).hexdigest(), "3eec807f0b21c25a535b31a4f09a0881")
 
-
+    def pedestrian_graph_c_ok_test(self):
+        self.assertEquals(hashlib.md5(open("data/karlsruhe_small.pypgrc", 'rb').read()).hexdigest(), "bea4d974990b5e9f022e57903ae0baa5")

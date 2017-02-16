03ceb53dda15cf03acf08ac8cdaf33754d0652ab
Andreas <andreasgemsa@gmx.de>
2017-02-16 08:08:04 +0100

    11-stability in integration test

--- graph/graph.py
+++ graph/graph.py
@@ -1,4 +1,5 @@
 import codecs
+import operator
 
 
 class Graph(object):
@@ -63,7 +64,8 @@ class Graph(object):
             f.write("{}\n".format(v.description))
 
         # write edge information
-        for e in self.edges:
+        sorted_edges = sorted(self.edges, key=lambda x: (x.s, x.t))
+        for e in sorted_edges:
             f.write("{}\n".format(e.description))
             f_names.write(e.name)
             f_names.write("\n")
--- tests/integration_test.py
+++ tests/integration_test.py
@@ -18,10 +18,10 @@ class IntegrationTest(unittest.TestCase):
         call(["python", "run.py", "-f", "data/karlsruhe_small.osm", "-n", "p", "-c"])
 
     def pedestrian_graph_ok_test(self):
-        self.assertEquals(hashlib.md5(open("data/karlsruhe_small.pypgr", 'rb').read()).hexdigest(), "d363bc83c99222ff9ddc9094e3f06664")
+        self.assertEquals(hashlib.md5(open("data/karlsruhe_small.pypgr", 'rb').read()).hexdigest(), "95353414f78a965fc8375e6df03c9910")
 
     def pedestrian_graph_names_ok_test(self):
-        self.assertEquals(hashlib.md5(open("data/karlsruhe_small.pypgr_names", 'rb').read()).hexdigest(), "2837fe0e24267d24a7b2e2335bcad687")
+        self.assertEquals(hashlib.md5(open("data/karlsruhe_small.pypgr_names", 'rb').read()).hexdigest(), "7bb07a5cb0d716332a787db0694201a9")
 
     @unittest.skip("testing skipping")
     def pedestrian_graph_c_names_ok_test(self):

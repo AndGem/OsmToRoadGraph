9d8bc324c4bd12a54b8217ad051586ebc3298eb3
AndGem <andreasgemsa@gmx.de>
2017-02-16 08:46:57 +0100

    11-integration test with small instance (#14)
    * 11-integration test with small instance
    
    * 11-missing dependency
    
    * 11-modified test
    
    * 11-disabled contracting
    
    * 11-another try
    
    * 11-stability in integration test
    
    * 11-initial integration test

---
+++ tests/integration_test.py
@@ -0,0 +1,39 @@
+import unittest
+from subprocess import call
+
+
+class IntegrationTest(unittest.TestCase):
+
+    ClassIsSetup = False
+
+    def setUp(self):
+        if not self.ClassIsSetup:
+            self.execute_program()
+            self.__class__.ClassIsSetup = True
+
+    def pedestrian_graph_ok_test(self):
+        nmb_nodes, nmb_edges = self.get_nmb_nodes_edges("data/karlsruhe_small.pypgr")
+
+        self.assertEquals(nmb_nodes, 4108)
+        self.assertEquals(nmb_edges, 4688)
+
+    def pedestrian_graph_contracted_ok_test(self):
+        nmb_nodes, nmb_edges = self.get_nmb_nodes_edges("data/karlsruhe_small.pypgrc")
+
+        self.assertEquals(nmb_nodes, 1319)
+        self.assertEquals(nmb_edges, 1886)
+
+    def execute_program(self):
+        call(["python", "run.py", "-f", "data/karlsruhe_small.osm", "-n", "p", "-c"])
+
+    def get_nmb_nodes_edges(self, path):
+        f = open(path)
+        line = f.readline()
+        print line
+        while '#' in line:
+            line = f.readline()
+
+        nmb_nodes = int(line)
+        line = f.readline()
+        nmb_edges = int(line)
+        return nmb_nodes, nmb_edges

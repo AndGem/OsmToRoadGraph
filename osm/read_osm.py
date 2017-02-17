45e0477d056b67cbc684b714a17701c08b704731
Andreas <andreasgemsa@gmx.de>
2017-02-17 23:01:12 +0100

    25 refactoring of read osm

--- osm/read_osm.py
+++ osm/read_osm.py
@@ -5,30 +5,32 @@ import utils.timer as timer
 
 
 @timer.timer(active=True)
-def _read_ways(osm_filename, configuration):
+def _read_ways(osm_file, configuration):
     parser = xml.sax.make_parser()
 
     w_handler = WayHandler(configuration)
 
     parser.setContentHandler(w_handler)
-    parser.parse(PercentageFile(osm_filename))
-    
+    parser.parse(PercentageFile(osm_file))
+
     return w_handler.found_ways, w_handler.found_nodes
-    
+
+
 @timer.timer(active=True)
-def _read_nodes(osm_filename, configuration, found_nodes):    
+def _read_nodes(osm_file, found_nodes):
     parser = xml.sax.make_parser()
     n_handler = NodeHandler(found_nodes)
 
     parser.setContentHandler(n_handler)
-    parser.parse(PercentageFile(osm_filename))
-    
+    parser.parse(osm_file)
+
     return n_handler.nodes
+
 
 @timer.timer(active=True)
 def read_file(osm_filename, configuration, verbose=True):
 
-    ways, found_node_ids = _read_ways(osm_filename, configuration)
-    nodes = _read_nodes(osm_filename, configuration, found_node_ids)
+    ways, found_node_ids = _read_ways(PercentageFile(osm_filename), configuration)
+    nodes = _read_nodes(PercentageFile(osm_filename), found_node_ids)
 
     return nodes, ways

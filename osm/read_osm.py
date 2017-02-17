37503856d6304028e2a46727353b985b90b38337
AndGem <andreasgemsa@gmx.de>
2017-02-17 18:05:23 +0100

    Update read_osm.py

--- osm/read_osm.py
+++ osm/read_osm.py
@@ -1,36 +1,34 @@
-import time
 import xml.sax
+
 from osm.xml_handler import NodeHandler, WayHandler, PercentageFile
+import utils.timer as timer
 
 
-def read_file(osm_filename, configuration, verbose=True):
-
-    def print_verbose():
-        if verbose:
-            print("done!")
-            print("{}s".format(time.time() - start_time))
-
+@timer.timer(active=True)
+def _read_ways(osm_filename, configuration):
     parser = xml.sax.make_parser()
 
-    start_time = time.time()
-    print("identifying all ways ...")
     w_handler = WayHandler(configuration)
 
     parser.setContentHandler(w_handler)
     parser.parse(PercentageFile(osm_filename))
-    print_verbose()
-
-    ways = w_handler.found_ways
-    found_nodes = w_handler.found_nodes
-
-    start_time = time.time()
-    print("\nidentifying all nodes ...")
+    
+    return w_handler.found_ways, w_handler.found_nodes
+    
+@timer.timer(active=True)
+def _read_nodes(osm_filename, configuration, found_nodes):    
+    parser = xml.sax.make_parser()
     n_handler = NodeHandler(found_nodes)
 
     parser.setContentHandler(n_handler)
     parser.parse(PercentageFile(osm_filename))
-    print_verbose()
+    
+    return n_handler.nodes
 
-    nodes = n_handler.nodes
+@timer.timer(active=True)
+def read_file(osm_filename, configuration, verbose=True):
+
+    ways, found_node_ids = _read_ways(osm_filename, configuration)
+    nodes = _read_nodes(osm_filename, configuration, found_node_ids)
 
     return nodes, ways

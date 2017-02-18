fb94df901fddcf9aed0141c8f22297299e8ef835
Andreas <andreasgemsa@gmx.de>
2017-02-18 22:28:03 +0100

    9 fix

--- tests/integration_test.py
+++ tests/integration_test.py
@@ -5,19 +5,9 @@ from subprocess import call
 class IntegrationTest(unittest.TestCase):
 
     def test_python(self):
-        self._execute_program("python")
+        self._execute_program()
         self.pedestrian_graph_ok()
         self.pedestrian_graph_contracted_ok()
-
-    def test_pypy(self):
-        self._execute_program("pypy")
-        self.pedestrian_graph_ok()
-        self.pedestrian_graph_contracted_ok()
-
-    # def test_python3(self):
-    #     self._execute_program("python3")
-    #     self.pedestrian_graph_ok()
-    #     self.pedestrian_graph_contracted_ok()
 
     def pedestrian_graph_ok(self):
         nmb_nodes, nmb_edges = self._get_nmb_nodes_edges("data/karlsruhe_small.pypgr")
@@ -31,8 +21,8 @@ class IntegrationTest(unittest.TestCase):
         self.assertEquals(nmb_nodes, 1319)
         self.assertEquals(nmb_edges, 1886)
 
-    def _execute_program(self, executable):
-        returncode = call([executable, "run.py", "-f", "data/karlsruhe_small.osm", "-n", "p", "-c"])
+    def _execute_program(self):
+        returncode = call(["python", "run.py", "-f", "data/karlsruhe_small.osm", "-n", "p", "-c"])
         self.assertEqual(0, returncode)
 
     def _get_nmb_nodes_edges(self, path):

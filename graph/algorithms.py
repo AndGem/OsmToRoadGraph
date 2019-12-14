c1573fad938aef31d065052ef1561be8d304e4a2
Andreas <andreas.gemsa@googlemail.com>
2019-12-14 17:25:42 +0100

    type information added

--- graph/algorithms.py
+++ graph/algorithms.py
@@ -5,7 +5,9 @@ import graph.graphfactory as graphfactory
 import utils.timer as timer
 
 
-def BFS(graph, s):
+from graph.graph import Graph
+from typing import Set
+def BFS(graph: Graph, s: int) -> Set[int]:
     seen_nodes = set([s])
     unvisited_nodes = deque([s])
 
@@ -36,7 +38,7 @@ def computeLCC(graph):
     return lcc
 
 
-def computeLCCGraph(graph):
+def computeLCCGraph(graph: Graph) -> Graph:
     lcc = computeLCC(graph)
     new_nodes = [graph.vertices[id] for id in lcc]
     return graphfactory.build_graph_from_vertices_edges(new_nodes, graph.edges)
--- osm/osm_types.py
+++ osm/osm_types.py
@@ -2,7 +2,7 @@
 class OSMWay:
     __slots__ = ["osm_id", "nodes", "highway", "area", "max_speed", "direction", "forward", "backward", "name"]
 
-    def __init__(self, osm_id):
+    def __init__(self, osm_id: int) -> None:
         self.osm_id = osm_id
         self.nodes = []
         self.highway = None
@@ -13,13 +13,13 @@ class OSMWay:
         self.backward = True
         self.name = ""
 
-    def add_node(self, osm_id):
+    def add_node(self, osm_id: int) -> None:
         self.nodes.append(osm_id)
 
 
 class OSMNode(object):
     __slots__ = ["lat", "lon", "osm_id"]
 
-    def __init__(self, osm_id, lat=None, lon=None):
+    def __init__(self, osm_id: int, lat: Optional[float] = None, lon: Optional[float] = None) -> None:
         self.osm_id = osm_id
         self.lat, self.lon = lat, lon

dd9526fab6cd25df8ed18770f13b94c55dab7331
Andreas <andreas.gemsa@googlemail.com>
2020-02-23 16:23:21 +0100

    added converting to networkx logic

---
+++ graph/convert_graph.py
@@ -0,0 +1,16 @@
+
+def convert_to_networkx(graph):
+    import networkx as nx
+    out_graph = nx.DiGraph()
+
+    for v in graph.vertices:
+        data_dict = {s: getattr(v.data, s) for s in v.data.__slots__}
+        out_graph.add_node(v.id, **data_dict)
+
+    for e in graph.edges:
+        data_dict = {s: getattr(e.data, s) for s in e.data.__slots__}
+        out_graph.add_edge(e.s, e.t, **data_dict)
+        if e.backward:
+            out_graph.add_edge(e.t, e.s, **data_dict)
+
+    return out_graph

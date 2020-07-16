7624216279b3bc2c90020ebb21a82ba8e742fe88
andreas <andreas.gemsa@googlemail.com>
2020-07-16 06:48:30 +0000

    added bz2 support

--- osm/read_osm.py
+++ osm/read_osm.py
@@ -1,3 +1,4 @@
+import bz2
 import xml.sax
 
 from osm.osm_types import OSMNode, OSMWay
@@ -13,10 +14,10 @@ from zipfile import ZipFile, BadZipFile
 def read_file(osm_filename, configuration, verbose=True) -> Tuple[Dict[int, OSMNode], List[OSMWay]]:
 
     parserHelper = WayParserHelper(configuration)
-    zip_content = read_zipfile(osm_filename)
-    if zip_content:
-        ways, found_node_ids = _read_ways(zip_content, parserHelper)
-        nodes = _read_nodes(zip_content, found_node_ids)
+    compressed_content = uncompress_content(osm_filename)
+    if compressed_content:
+        ways, found_node_ids = _read_ways(compressed_content, parserHelper)
+        nodes = _read_nodes(compressed_content, found_node_ids)
     else:
         ways, found_node_ids = _read_ways(PercentageFile(osm_filename), parserHelper)
         nodes = _read_nodes(PercentageFile(osm_filename), found_node_ids)
@@ -24,17 +25,30 @@ def read_file(osm_filename, configuration, verbose=True) -> Tuple[Dict[int, OSMN
     return nodes, ways
 
 
-def read_zipfile(osm_filename):
-    try:
-        zipfile = ZipFile(osm_filename)
-        osm_file_in_zip = None
-        for f in zipfile.infolist():
-            if f.filename.endswith(".osm"):
-                osm_file_in_zip = f.filename
-                break
-        return zipfile.read(name=osm_file_in_zip)
+def uncompress_content(osm_filename):
+    magic_bz2 = "\x42\x5a\x68"
+    magic_zip = "\x50\x4b\x03\x04"
 
-    except BadZipFile:
+    with open(osm_filename) as f:
+        content_begin = f.read(max(len(x) for x in [magic_bz2, magic_zip]))
+        if content_begin.startswith(magic_bz2):
+            print("identified bz2 compressed file.. decompressing")
+            content = bz2.read()
+            print("done!")
+            return content
+        elif content_begin.startswith(magic_zip):
+            print("identified bz2 compressed file.. decompressing")
+            zipfile = ZipFile(osm_filename)
+            osm_file_in_zip = None
+            for f in zipfile.infolist():
+                if f.filename.endswith(".osm"):
+                    osm_file_in_zip = f.filename
+                    break
+            content = zipfile.read(name=osm_file_in_zip)
+            print("done!")
+            return content
+
+        print("no compression recognized!")
         return None
 
 
